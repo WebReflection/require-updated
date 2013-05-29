7deda25633240f0e1959b0f1e6a2eaf9b5502075
Andrea Giammarchi <agiammarchi@twitter.com>
2013-05-29 08:32:38 -0700

    trying to fix travis problems with paths

--- test/require-updated.js
+++ test/require-updated.js
@@ -1,5 +1,7 @@
 //remove:
 var ru = require('../build/require-updated.node.js');
+var samePath = __dirname == process.cwd();
+console.log(samePath);
 //:remove
 
 wru.test([
@@ -17,22 +19,23 @@ wru.test([
     name: 'cache files and auto updates',
     test: function () {
       var fs = ru('fs'),
-          tmp1 = '/tmp1.js',
-          tmp2 = '/tmp2.js';
+          tmp1 = 'tmp1.js',
+          tmp2 = 'tmp2.js',
+          path = samePath ? '.' : '..';
       fs.writeFileSync('./' + tmp1, 'module.exports=Math.random()', 'utf-8');
       fs.writeFileSync('./' + tmp2, 'module.exports=Math.random()', 'utf-8');
       // be sure no other things happen to those files, exit current IDLE
       setTimeout(wru.async(function(){
-         var  module1 = ru('../' + tmp1),
-              module2 = ru('../' + tmp2);
-          wru.assert('same result', module1 === ru('../' + tmp1) &&
-                                    module2 === ru('../' + tmp2));
+         var  module1 = ru(path + '/' + tmp1),
+              module2 = ru(path + '/' + tmp2);
+          wru.assert('same result', module1 === ru(path + '/' + tmp1) &&
+                                    module2 === ru(path + '/' + tmp2));
           fs.writeFileSync('./' + tmp1, 'module.exports=Math.random()', 'utf-8');
           setTimeout(wru.async(function(){
-            var newResult = ru('../' + tmp1);
+            var newResult = ru(path + '/' + tmp1);
             wru.assert('file changed so will the result', module1 !== newResult);
-            wru.assert('still cache works', newResult === ru('../' + tmp1));
-            wru.assert('what was cached still is', module2 === ru('../' + tmp2));
+            wru.assert('still cache works', newResult === ru(path + '/' + tmp1));
+            wru.assert('what was cached still is', module2 === ru(path + '/' + tmp2));
             fs.unlinkSync('./' + tmp1);
             fs.unlinkSync('./' + tmp2);
           }), 1500);
@@ -42,11 +45,12 @@ wru.test([
     name: 'nasty oprations outside the module',
     test: function () {
       ru('fs').writeFileSync('./tmp1.js', 'module.exports=Math.random()', 'utf-8');
-      var  module1 = ru('../tmp1.js');
-      wru.assert('same results', module1 === ru('../tmp1.js'));
-      wru.assert('removing the cache', delete require.cache[require.resolve('../tmp1.js')]);
-      wru.assert('reloaded', module1 !== ru('../tmp1.js'));
-      wru.assert('re-cached', ru('../tmp1.js') === ru('../tmp1.js'));
+      var path = samePath ? '.' : '..',
+          module1 = ru(path + '/' + 'tmp1.js');
+      wru.assert('same results', module1 === ru(path + '/' + 'tmp1.js'));
+      wru.assert('removing the cache', delete require.cache[require.resolve(path + '/' + 'tmp1.js')]);
+      wru.assert('reloaded', module1 !== ru(path + '/' + 'tmp1.js'));
+      wru.assert('re-cached', ru(path + '/' + 'tmp1.js') === ru(path + '/' + 'tmp1.js'));
       ru('fs').unlinkSync('./tmp1.js');
       setTimeout(wru.async(wru.assert), 1500, true);
     }
