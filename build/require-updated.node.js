b5c20638be4fdd62a3712953956b0a434ddbb391
Andrea Giammarchi <agiammarchi@twitter.com>
2013-05-31 16:04:50 -0700

    better solution for external inclusion

--- build/require-updated.node.js
+++ build/require-updated.node.js
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-module.exports = function(re, fs){
+module.exports = function(re, fs, path){
   // (C) WebReflection - Mit Style License
   function watcher(
     /* event, filename */
@@ -28,13 +28,18 @@ module.exports = function(re, fs){
     this.close();
     delete require.cache[this.filename];
   }
+  var dirName = path.dirname(module.parent.filename);
   return function requireUpdated(filename) {
     return re.test(filename) && (
-      (filename = require.resolve(filename)) in require.cache ||
+      (filename = require.resolve(
+        path.resolve(dirName, filename))
+      ) in require.cache ||
       (fs.watch(filename, watcher).filename = filename)
     ), require(filename);
   };
 }(
   /^(?:\.|\/|\\|[A-Z]:)/,
-  require('fs')
+  require('fs'),
+  require('path')
 );
+delete require.cache[__filename];
--- src/require-updated.js
+++ src/require-updated.js
@@ -1,4 +1,4 @@
-function(re, fs){
+function(re, fs, path){
   // (C) WebReflection - Mit Style License
   function watcher(
     /* event, filename */
@@ -6,13 +6,17 @@ function(re, fs){
     this.close();
     delete require.cache[this.filename];
   }
+  var dirName = path.dirname(module.parent.filename);
   return function requireUpdated(filename) {
     return re.test(filename) && (
-      (filename = require.resolve(filename)) in require.cache ||
+      (filename = require.resolve(
+        path.resolve(dirName, filename))
+      ) in require.cache ||
       (fs.watch(filename, watcher).filename = filename)
     ), require(filename);
   };
 }(
   /^(?:\.|\/|\\|[A-Z]:)/,
-  require('fs')
+  require('fs'),
+  require('path')
 )
